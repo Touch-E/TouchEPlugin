ec606eac7a15d8331653c50fd2de0f7240632c52
kishan <kishan.italiya@checker-solutions.com>
2024-06-13 17:33:53 +0530

    - SDK Update

--- Example/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let urlString = "https://api-cluster.system.touchetv.com"
-        var userToken = ""//"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0MSIsInVzZXJSb2xlcyI6WyJWSUVXRVIiXSwib3JnSWRzIjpbMV0sImlhdCI6MTcwNzI3OTE4MSwiZXhwIjoxNzA3MzY5MTgxfQ.vLJ73Ei6jklWSCPGHY54Tpluom1hgCH0cgXPjrvjrPvdg8792SJOWlFx8ojpOAF1-ZVP8cdGqwGmkfMse8n9ow"
+        var userToken = ""
         if let retrievedString = UserDefaults.standard.string(forKey: "userToken") {
             userToken = retrievedString
         } else {
@@ -52,7 +52,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return OrientationManager.shared.orientationHandler.supportedInterfaceOrientations()
+    }
 
 }
+
 
